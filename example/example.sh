35d905041ba78a99bed779bcda327253e44d52bd
high on tantor <josephvenable@verizon.net>
2026-05-21 02:19:53 -0400

    Update example.sh

--- example/example.sh
+++ example/example.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
-cd "${0%/*}"
+cd "${0%/*}/../"
 if [ $(uname) = "Darwin" ]; then
-  clang++ example.cpp ../process.cpp -o ../xproc -std=c++17 -DNULLIFY_STDERR -Wall -arch arm64 -arch x86_64; ../xproc;
+  clang++ example.cpp process.cpp -o xproc -std=c++17 -DNULLIFY_STDERR -Wall -arch arm64 -arch x86_64; ./xproc;
 elif [ $(uname) = "Linux" ]; then
   if [ -f "/bin/g++" ]; then
-    g++ example.cpp ../process.cpp -o ../xproc -std=c++17 -DNULLIFY_STDERR -Wall -static-libgcc -static-libstdc++ -static; ../xproc;
+    g++ example.cpp process.cpp -o xproc -std=c++17 -DNULLIFY_STDERR -Wall -static-libgcc -static-libstdc++ -static; ./xproc;
   else
-    clang++ example.cpp ../process.cpp -o ../xproc -std=c++17 -DNULLIFY_STDERR -Wall; ../xproc;
+    clang++ example.cpp process.cpp -o xproc -std=c++17 -DNULLIFY_STDERR -Wall; ./xproc;
   fi
 elif [ $(uname) = "FreeBSD" ]; then
-  clang++ example.cpp ../process.cpp -o ../xproc -std=c++17 -DNULLIFY_STDERR -Wall -lelf -lkvm -lpthread -static; ../xproc;
+  clang++ example.cpp process.cpp -o xproc -std=c++17 -DNULLIFY_STDERR -Wall -lelf -lkvm -lpthread -static; ./xproc;
 elif [ $(uname) = "DragonFly" ]; then
-  g++ example.cpp ../process.cpp -o ../xproc -std=c++17 -DNULLIFY_STDERR -Wall -static-libgcc -static-libstdc++ -lkvm -lpthread -static; ../xproc;
+  g++ example.cpp process.cpp -o xproc -std=c++17 -DNULLIFY_STDERR -Wall -static-libgcc -static-libstdc++ -lkvm -lpthread -static; ./xproc;
 elif [ $(uname) = "NetBSD" ]; then
-  g++ example.cpp ../process.cpp -o ../xproc -std=c++17 -DNULLIFY_STDERR -Wall -static-libgcc -static-libstdc++ -lkvm -lpthread -static; ../xproc;
+  g++ example.cpp process.cpp -o xproc -std=c++17 -DNULLIFY_STDERR -Wall -static-libgcc -static-libstdc++ -lkvm -lpthread -static; ./xproc;
 elif [ $(uname) = "OpenBSD" ]; then
-  clang++ example.cpp ../process.cpp -o ../xproc -std=c++17 -DNULLIFY_STDERR -Wall -lkvm -lpthread -static; ../xproc;
+  clang++ example.cpp process.cpp -o xproc -std=c++17 -DNULLIFY_STDERR -Wall -lkvm -lpthread -static; ./xproc;
 elif [ $(uname) = "SunOS" ]; then
   if [ $(uname -o) = "illumos" ]; then
     export SOLARIS=0;
@@ -23,10 +23,10 @@ elif [ $(uname) = "SunOS" ]; then
     export SOLARIS=1;
   fi
   if [ $SOLARIS = 0 ]; then 
-    g++ example.cpp ../process.cpp -o ../xproc -std=c++17 -DNULLIFY_STDERR -D__illumos__ -Wall -lkvm -lproc -static-libgcc; ../xproc;
+    g++ example.cpp process.cpp -o xproc -std=c++17 -DNULLIFY_STDERR -D__illumos__ -Wall -lkvm -lproc -static-libgcc; ./xproc;
   else
-    g++ example.cpp ../process.cpp -o ../xproc -std=c++17 -DNULLIFY_STDERR -Wall -lkvm -lproc -static-libgcc; ../xproc;
+    g++ example.cpp process.cpp -o xproc -std=c++17 -DNULLIFY_STDERR -Wall -lkvm -lproc -static-libgcc; ./xproc;
   fi
 else
-  g++ example.cpp ../process.cpp -o ../xproc.exe -std=c++17 -DNULLIFY_STDERR -Wall -static-libgcc -static-libstdc++ -static -lntdll -Wl,--subsystem,console; ../xproc.exe;
+  g++ example.cpp process.cpp -o xproc.exe -std=c++17 -DNULLIFY_STDERR -Wall -static-libgcc -static-libstdc++ -static -lntdll -Wl,--subsystem,console; ./xproc.exe;
 fi

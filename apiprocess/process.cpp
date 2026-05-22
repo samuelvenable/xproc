dfb668b8439c5d0f66ba9e912e1ba2e21f66bd42
high on tantor <josephvenable@verizon.net>
2026-05-21 22:38:44 -0400

    Update process.cpp

--- apiprocess/process.cpp
+++ apiprocess/process.cpp
@@ -37,9 +37,9 @@
 #if (!defined(TARGET_OS_OSX) || !TARGET_OS_OSX)
 #error "Unsupported Platform! Supported Platforms: Windows, macOS, Linux, FreeBSD, DragonFly BSD, NetBSD, OpenBSD, Solaris, illumos, and Android."
 #endif
+#endif
 #else
 #error "Unsupported Platform! Supported Platforms: Windows, macOS, Linux, FreeBSD, DragonFly BSD, NetBSD, OpenBSD, Solaris, illumos, and Android."
-#endif
 #endif
 #if ((defined(_WIN32) || defined(_WIN64)) || (defined(__APPLE__) && defined(__MACH__)) || (defined(__linux__) || defined(__ANDROID__)) || (defined(__FreeBSD__) || defined(__FreeBSD_kernel__)) || defined(__DragonFly__) || defined(__NetBSD__) || defined(__OpenBSD__) || (defined(__sun) && defined(__SVR4)))
 #include <unordered_map>

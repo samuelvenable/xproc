bd348474fc80290414b33cbc5e8e5b03f9359cb6
high on tantor <josephvenable@verizon.net>
2026-05-23 01:06:37 -0400

    Update process.hpp

--- apiprocess/process.hpp
+++ apiprocess/process.hpp
@@ -29,7 +29,7 @@
 #if ((defined(_WIN32) || defined(_WIN64)) || (defined(__APPLE__) && defined(__MACH__)) || (defined(__linux__) || defined(__ANDROID__)) || (defined(__FreeBSD__) || defined(__FreeBSD_kernel__)) || defined(__DragonFly__) || defined(__NetBSD__) || defined(__OpenBSD__) || (defined(__sun) && defined(__SVR4)))
 #include <cstdint>
 #if (defined(__sun) && defined(__SVR4))
-#if (INTPTR_MAX != INT64_MAX)
+#if ((defined(INTPTR_MAX) && defined(INT64_MAX)) && INTPTR_MAX != INT64_MAX)
 #error "Unsupported Platform! Only 64-bit Architectures are Supported on Solaris and illumos."
 #endif
 #endif
@@ -42,7 +42,7 @@
 #else
 #error "Unsupported Platform! Supported Platforms: Windows, macOS, Linux, FreeBSD, DragonFly BSD, NetBSD, OpenBSD, Solaris, illumos, and Android."
 #endif
-#if ((defined(_WIN32) || defined(_WIN64)) || ((defined(__APPLE__) && defined(__MACH__)) && (defined(TARGET_OS_OSX) && TARGET_OS_OSX)) || (defined(__linux__) || defined(__ANDROID__)) || (defined(__FreeBSD__) || defined(__FreeBSD_kernel__)) || defined(__DragonFly__) || defined(__NetBSD__) || defined(__OpenBSD__) || ((defined(__sun) && defined(__SVR4)) && (INTPTR_MAX == INT64_MAX)))
+#if ((defined(_WIN32) || defined(_WIN64)) || ((defined(__APPLE__) && defined(__MACH__)) && (defined(TARGET_OS_OSX) && TARGET_OS_OSX)) || (defined(__linux__) || defined(__ANDROID__)) || (defined(__FreeBSD__) || defined(__FreeBSD_kernel__)) || defined(__DragonFly__) || defined(__NetBSD__) || defined(__OpenBSD__) || ((defined(__sun) && defined(__SVR4)) && ((defined(INTPTR_MAX) && defined(INT64_MAX)) && (INTPTR_MAX == INT64_MAX))))
 #include <vector>
 #include <string>
 

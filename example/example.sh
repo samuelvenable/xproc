b8dbc915933a43978e46d4509e16b74d451132f8
high on tantor <josephvenable@verizon.net>
2026-05-23 01:16:41 -0400

    Update example.sh

--- example/example.sh
+++ example/example.sh
@@ -1,32 +1,27 @@
 #!/bin/sh
 cd "${0%/*}/../"
-if [ $(uname) = "Darwin" ]; then
-  clang++ example/example.cpp apiprocess/process.cpp -o xproc -std=c++17 -DNULLIFY_STDERR -Wall -arch arm64 -arch x86_64; ./xproc;
+if [ $(uname -o) = "Msys" ]; then
+  g++ example/example.cpp apiprocess/process.cpp -o xproc.exe -I. -std=c++17 -DNULLIFY_STDERR -Wall -static-libgcc -static-libstdc++ -static -lntdll -Wl,--subsystem,console; ./xproc.exe;
+elif [ $(uname) = "Darwin" ]; then
+  clang++ example/example.cpp apiprocess/process.cpp -o xproc -I.-std=c++17 -DNULLIFY_STDERR -Wall -arch arm64 -arch x86_64; ./xproc;
 elif [ $(uname) = "Linux" ]; then
   if [ -f "/bin/g++" ]; then
-    g++ example/example.cpp apiprocess/process.cpp -o xproc -std=c++17 -DNULLIFY_STDERR -Wall -static-libgcc -static-libstdc++ -static; ./xproc;
+    g++ example/example.cpp apiprocess/process.cpp -o xproc -I.-std=c++17 -DNULLIFY_STDERR -Wall -static-libgcc -static-libstdc++ -static; ./xproc;
   else
-    clang++ example/example.cpp apiprocess/process.cpp -o xproc -std=c++17 -DNULLIFY_STDERR -Wall; ./xproc;
-  fi
+    clang++ example/example.cpp apiprocess/process.cpp -o xproc -I.-std=c++17 -DNULLIFY_STDERR -Wall; ./xproc;
+  fi;
 elif [ $(uname) = "FreeBSD" ]; then
-  clang++ example/example.cpp apiprocess/process.cpp -o xproc -std=c++17 -DNULLIFY_STDERR -Wall -lelf -lkvm -lpthread -static; ./xproc;
+  clang++ example/example.cpp apiprocess/process.cpp -o xproc -I.-std=c++17 -DNULLIFY_STDERR -Wall -lelf -lkvm -lpthread -static; ./xproc;
 elif [ $(uname) = "DragonFly" ]; then
-  g++ example/example.cpp apiprocess/process.cpp -o xproc -std=c++17 -DNULLIFY_STDERR -Wall -static-libgcc -static-libstdc++ -lkvm -lpthread -static; ./xproc;
+  g++ example/example.cpp apiprocess/process.cpp -o xproc -I.-std=c++17 -DNULLIFY_STDERR -Wall -static-libgcc -static-libstdc++ -lkvm -lpthread -static; ./xproc;
 elif [ $(uname) = "NetBSD" ]; then
-  g++ example/example.cpp apiprocess/process.cpp -o xproc -std=c++17 -DNULLIFY_STDERR -Wall -static-libgcc -static-libstdc++ -lkvm -lpthread -static; ./xproc;
+  g++ example/example.cpp apiprocess/process.cpp -o xproc -I.-std=c++17 -DNULLIFY_STDERR -Wall -static-libgcc -static-libstdc++ -lkvm -lpthread -static; ./xproc;
 elif [ $(uname) = "OpenBSD" ]; then
-  clang++ example/example.cpp apiprocess/process.cpp -o xproc -std=c++17 -DNULLIFY_STDERR -Wall -lkvm -lpthread -static; ./xproc;
+  clang++ example/example.cpp apiprocess/process.cpp -o xproc -I.-std=c++17 -DNULLIFY_STDERR -Wall -lkvm -lpthread -static; ./xproc;
 elif [ $(uname) = "SunOS" ]; then
   if [ $(uname -o) = "illumos" ]; then
-    export SOLARIS=0;
+    g++ example/example.cpp apiprocess/process.cpp -o xproc -I.-std=c++17 -DNULLIFY_STDERR -D__illumos__ -Wall -lkvm -lproc -static-libgcc; ./xproc;
   else
-    export SOLARIS=1;
-  fi
-  if [ $SOLARIS = 0 ]; then 
-    g++ example/example.cpp apiprocess/process.cpp -o xproc -std=c++17 -DNULLIFY_STDERR -D__illumos__ -Wall -lkvm -lproc -static-libgcc; ./xproc;
-  else
-    g++ example/example.cpp apiprocess/process.cpp -o xproc -std=c++17 -DNULLIFY_STDERR -Wall -lkvm -lproc -static-libgcc; ./xproc;
-  fi
-else
-  g++ example/example.cpp apiprocess/process.cpp -o xproc.exe -std=c++17 -DNULLIFY_STDERR -Wall -static-libgcc -static-libstdc++ -static -lntdll -Wl,--subsystem,console; ./xproc.exe;
-fi
+    g++ example/example.cpp apiprocess/process.cpp -o xproc -I.-std=c++17 -DNULLIFY_STDERR -Wall -lkvm -lproc -static-libgcc; ./xproc;
+  fi;
+fi;

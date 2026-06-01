e53d04cbde237b9eb55348f0e3da541495fa318a
high on tantor <josephvenable@verizon.net>
2026-06-01 17:07:10 -0400

    Update process.cpp

--- apiprocess/process.cpp
+++ apiprocess/process.cpp
@@ -241,7 +241,7 @@ namespace {
     }
     if (!resolved_path && wcslen(buf)) {
       return _wcsdup(result.c_str());
-    } else if (resolved_path && wcslen(result.c_str())) {
+    } else if (resolved_path && !result.empty()) {
       wcsncpy_s(ptr, MAX_PATH, result.c_str(), _TRUNCATE);
       return (wchar_t *)ptr;
     }

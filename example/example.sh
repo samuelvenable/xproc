17b99915dfdef57f35001707cc9209bc591ee9cf
high on tantor <josephvenable@verizon.net>
2026-06-21 20:02:03 -0400

    Update example.sh

--- example/example.sh
+++ example/example.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-cd "${0%/*}/../"
 # Some platforms do not accept the -o flag for uname; any warnings printed to the terminal concerning this can be safely ignored...
+cd "${0%/*}/../";
 if [ `uname -o` = "Msys" ]; then
   g++ example/example.cpp apiprocess/process.cpp -o xproc.exe -I. -std=c++17 -DNULLIFY_STDERR -Wall -static-libgcc -static-libstdc++ -static -lntdll -Wl,--subsystem,console; ./xproc.exe;
 elif [ `uname` = "Darwin" ]; then
-  clang++ example/example.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -Wall -arch arm64 -arch x86_64; ./xproc;
+  clang++ example/example.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -Wall -mmacos-version-min=10.13 -arch arm64 -arch x86_64; ./xproc;
 elif [ `uname -o` = "GNU/Linux" ]; then
   if [ -f "/bin/g++" ]; then
     g++ example/example.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -Wall -static-libgcc -static-libstdc++ -static; ./xproc;
@@ -21,8 +21,8 @@ elif [ `uname` = "OpenBSD" ]; then
   clang++ example/example.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -Wall -lkvm -lpthread -static; ./xproc;
 elif [ `uname` = "SunOS" ]; then
   if [ `uname -o` = "illumos" ]; then
-    g++ example/example.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -D__illumos__ -Wall -lkvm -lproc -static-libgcc; ./xproc;
+    g++ example/example.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -D__illumos__ -Wall -static-libgcc -lkvm -lproc; ./xproc;
   else
-    g++ example/example.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -Wall -lkvm -lproc -static-libgcc; ./xproc;
+    g++ example/example.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -Wall -static-libgcc -lkvm -lproc; ./xproc;
   fi;
 fi;

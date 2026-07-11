f0c594dc8377e7925f28ab524641f6295b13a048
high on tantor <josephvenable@verizon.net>
2026-07-11 17:28:39 -0400

    Update example.sh

--- example/example.sh
+++ example/example.sh
@@ -2,7 +2,7 @@
 # Some platforms do not accept the -o flag for uname; any warnings printed to the terminal concerning this can be safely ignored...
 cd "${0%/*}/../";
 if [ `uname -o` = "Msys" ]; then
-  g++ example/example.cpp apiprocess/process.cpp -o xproc.exe -I. -std=c++17 -DNULLIFY_STDERR -Wall -fsanitize=undefined -fno-sanitize-recover=undefined -g -ggdb -static-libgcc -static-libstdc++ -static -lntdll -Wl,--subsystem,console; ./xproc.exe;
+  clang++ example/example.cpp apiprocess/process.cpp -o xproc.exe -I. -std=c++17 -DNULLIFY_STDERR -Wall -fsanitize=undefined -fno-sanitize-recover=undefined -g -ggdb -static-libgcc -static-libstdc++ -static -lntdll -Wl,--subsystem,console; ./xproc.exe;
 elif [ `uname` = "Darwin" ]; then
   clang++ example/example.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -Wall -fsanitize=undefined -fno-sanitize-recover=undefined -g -ggdb -mmacos-version-min=10.13 -arch arm64 -arch x86_64; ./xproc;
 elif [ `uname -o` = "GNU/Linux" ]; then

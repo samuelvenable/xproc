d090c3fb74795f5e9a25d6aa1a54efd58985f7d5
high on tantor <josephvenable@verizon.net>
2026-07-11 17:06:51 -0400

    Update example.sh

--- example/example.sh
+++ example/example.sh
@@ -2,27 +2,27 @@
 # Some platforms do not accept the -o flag for uname; any warnings printed to the terminal concerning this can be safely ignored...
 cd "${0%/*}/../";
 if [ `uname -o` = "Msys" ]; then
-  g++ example/example.cpp apiprocess/process.cpp -o xproc.exe -I. -std=c++17 -DNULLIFY_STDERR -Wall -Wextra -Wpedantic -Werror -fsanitize=undefined -fno-sanitize-recover=undefined -g -ggdb -static-libgcc -static-libstdc++ -static -lntdll -Wl,--subsystem,console; ./xproc.exe;
+  g++ example/example.cpp apiprocess/process.cpp -o xproc.exe -I. -std=c++17 -DNULLIFY_STDERR -Wall -Wextra -Wpedantic -fsanitize=undefined -fno-sanitize-recover=undefined -g -ggdb -static-libgcc -static-libstdc++ -static -lntdll -Wl,--subsystem,console; ./xproc.exe;
 elif [ `uname` = "Darwin" ]; then
-  clang++ example/example.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -Wall -Wextra -Wpedantic -Werror -fsanitize=undefined -fno-sanitize-recover=undefined -g -ggdb -mmacos-version-min=10.13 -arch arm64 -arch x86_64; ./xproc;
+  clang++ example/example.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -Wall -Wextra -Wpedantic -fsanitize=undefined -fno-sanitize-recover=undefined -g -ggdb -mmacos-version-min=10.13 -arch arm64 -arch x86_64; ./xproc;
 elif [ `uname -o` = "GNU/Linux" ]; then
   if [ -f "/bin/g++" ]; then
-    g++ example/example.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -Wall -Wextra -Wpedantic -Werror -fsanitize=undefined -fno-sanitize-recover=undefined -g -ggdb -static-libgcc -static-libstdc++ -static; ./xproc;
+    g++ example/example.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -Wall -Wextra -Wpedantic -fsanitize=undefined -fno-sanitize-recover=undefined -g -ggdb -static-libgcc -static-libstdc++ -static; ./xproc;
   else
-    clang++ example/example.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -Wall -Wextra -Wpedantic -Werror -fsanitize=undefined -fno-sanitize-recover=undefined -g -ggdb; ./xproc;
+    clang++ example/example.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -Wall -Wextra -Wpedantic -fsanitize=undefined -fno-sanitize-recover=undefined -g -ggdb; ./xproc;
   fi;
 elif [ `uname` = "FreeBSD" ]; then
-  clang++ example/example.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -Wall -Wextra -Wpedantic -Werror -fsanitize=undefined -fno-sanitize-recover=undefined -g -ggdb -lelf -lkvm -lpthread -static; ./xproc;
+  clang++ example/example.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -Wall -Wextra -Wpedantic -fsanitize=undefined -fno-sanitize-recover=undefined -g -ggdb -lelf -lkvm -lpthread -static; ./xproc;
 elif [ `uname` = "DragonFly" ]; then
-  g++ example/example.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -Wall -Wextra -Wpedantic -Werror -fsanitize=undefined -fno-sanitize-recover=undefined -g -ggdb -static-libgcc -static-libstdc++ -lkvm -lpthread -static; ./xproc;
+  g++ example/example.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -Wall -Wextra -Wpedantic -fsanitize=undefined -fno-sanitize-recover=undefined -g -ggdb -static-libgcc -static-libstdc++ -lkvm -lpthread -static; ./xproc;
 elif [ `uname` = "NetBSD" ]; then
-  g++ example/example.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -Wall -Wextra -Wpedantic -Werror -fsanitize=undefined -fno-sanitize-recover=undefined -g -ggdb -static-libgcc -static-libstdc++ -lkvm -lpthread -static; ./xproc;
+  g++ example/example.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -Wall -Wextra -Wpedantic -fsanitize=undefined -fno-sanitize-recover=undefined -g -ggdb -static-libgcc -static-libstdc++ -lkvm -lpthread -static; ./xproc;
 elif [ `uname` = "OpenBSD" ]; then
-  clang++ example/example.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -Wall -Wextra -Wpedantic -Werror -fsanitize=undefined -fno-sanitize-recover=undefined -g -ggdb -lkvm -lpthread -static; ./xproc;
+  clang++ example/example.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -Wall -Wextra -Wpedantic -fsanitize=undefined -fno-sanitize-recover=undefined -g -ggdb -lkvm -lpthread -static; ./xproc;
 elif [ `uname` = "SunOS" ]; then
   if [ `uname -o` = "illumos" ]; then
-    g++ example/example.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -D__illumos__ -Wall -Wextra -Wpedantic -Werror -fsanitize=undefined -fno-sanitize-recover=undefined -g -ggdb -static-libgcc -lkvm -lproc; ./xproc;
+    g++ example/example.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -D__illumos__ -Wall -Wextra -Wpedantic -fsanitize=undefined -fno-sanitize-recover=undefined -g -ggdb -static-libgcc -lkvm -lproc; ./xproc;
   else
-    g++ example/example.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -Wall -Wextra -Wpedantic -Werror -fsanitize=undefined -fno-sanitize-recover=undefined -g -ggdb -static-libgcc -lkvm -lproc; ./xproc;
+    g++ example/example.cpp apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -Wall -Wextra -Wpedantic -fsanitize=undefined -fno-sanitize-recover=undefined -g -ggdb -static-libgcc -lkvm -lproc; ./xproc;
   fi;
 fi;

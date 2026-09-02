270c61892937f1b40107dc73dde9e9bc626f31c2
high on tantor <josephvenable@verizon.net>
2026-09-02 08:18:46 -0400

    Update process.cpp

--- apiprocess/process.cpp
+++ apiprocess/process.cpp
@@ -568,7 +568,7 @@ namespace ngs::ps {
     if (!kd) return vec;
     if ((proc_info = kvm_getprocs(kd, KERN_PROC_PROC, 0, &cntp))) {
       for (int i = 0; i < cntp; i++) {
-        if (proc_info[i].ki_flag & P_SYSTEM) {
+        if (proc_info[i].p_flag & P_SYSTEM) {
           continue;
         }
         vec.push_back(proc_info[i].ki_pid);
@@ -585,7 +585,7 @@ namespace ngs::ps {
     if (!kd) return vec;
     if ((proc_info = kvm_getprocs(kd, KERN_PROC_ALL, 0, &cntp))) {
       for (int i = 0; i < cntp; i++) {
-        if (proc_info[i].kp_flag & P_SYSTEM) {
+        if (proc_info[i].kp_flags & P_SYSTEM) {
           continue;
         }
         vec.push_back(proc_info[i].kp_pid);
@@ -776,7 +776,7 @@ namespace ngs::ps {
     kd = kvm_openfiles(nlistf, memf, nullptr, O_RDONLY, nullptr);
     if (!kd) return vec;
     if ((proc_info = kvm_getprocs(kd, KERN_PROC_PID, proc_id, &cntp))) {
-      if (!(proc_info->ki_flag & P_SYSTEM)) {
+      if (!(proc_info->p_flag & P_SYSTEM)) {
         vec.push_back(proc_info->ki_ppid);
       }
     }
@@ -790,7 +790,7 @@ namespace ngs::ps {
     kd = kvm_openfiles(nlistf, memf, nullptr, O_RDONLY, nullptr);
     if (!kd) return vec;
     if ((proc_info = kvm_getprocs(kd, KERN_PROC_PID, proc_id, &cntp))) {
-      if (!(proc_info->kp_flag & P_SYSTEM)) {
+      if (!(proc_info->kp_flags & P_SYSTEM)) {
         vec.push_back(proc_info->kp_ppid);
       }
     }
@@ -910,7 +910,7 @@ namespace ngs::ps {
     if (!kd) return vec;
     if ((proc_info = kvm_getprocs(kd, KERN_PROC_PROC, 0, &cntp))) {
       for (int i = 0; i < cntp; i++) {
-        if (proc_info[i].ki_flag & P_SYSTEM) {
+        if (proc_info[i].p_flag & P_SYSTEM) {
           continue;
         }
         if (proc_info[i].ki_ppid == parent_proc_id) {
@@ -929,7 +929,7 @@ namespace ngs::ps {
     if (!kd) return vec;
     if ((proc_info = kvm_getprocs(kd, KERN_PROC_ALL, 0, &cntp))) {
       for (int i = 0; i < cntp; i++) {
-        if (proc_info[i].kp_flag & P_SYSTEM) {
+        if (proc_info[i].kp_flags & P_SYSTEM) {
           continue;
         }
         if (proc_info[i].kp_ppid == parent_proc_id) {

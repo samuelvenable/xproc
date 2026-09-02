73f6ab9f40b081f058fb5e47650f409c3a7eee94
high on tantor <josephvenable@verizon.net>
2026-09-02 16:26:50 -0400

    Update process.cpp

--- apiprocess/process.cpp
+++ apiprocess/process.cpp
@@ -659,6 +659,14 @@ namespace ngs::ps {
     kvm_close(kd);
     finish:
     #endif
+    #if (defined(_WIN32) || defined(_WIN64))
+    // Removes a PID of four, (it is not a user-level process on Windows)...
+    auto itr = std::remove(vec.begin(), vec.end(), 4);
+    vec.erase(itr, vec.end());
+    #endif
+    // Removes a PID of zero, (it is not a user-level process)...
+    auto itr = std::remove(vec.begin(), vec.end(), 0);
+    vec.erase(itr, vec.end());
     std::sort(vec.begin(), vec.end());
     return vec;
   }
@@ -885,12 +893,12 @@ namespace ngs::ps {
     kvm_close(kd);
     finish:
     #endif
-	#if (defined(_WIN32) || defined(_WIN64))
+    #if (defined(_WIN32) || defined(_WIN64))
     // Removes a PID of four, (it is not a user-level process on Windows)...
     if (!vec.empty() && vec[0] == 4) {
       vec.clear();
     }
-	#endif
+    #endif
     // Removes a PID of zero, (it is not a user-level process)...
     if (!vec.empty() && vec[0] == 0) {
       vec.clear();
@@ -1055,6 +1063,14 @@ namespace ngs::ps {
     kvm_close(kd);
     finish:
     #endif
+    #if (defined(_WIN32) || defined(_WIN64))
+    // Removes a PID of four, (it is not a user-level process on Windows)...
+    auto itr = std::remove(vec.begin(), vec.end(), 4);
+    vec.erase(itr, vec.end());
+    #endif
+    // Removes a PID of zero, (it is not a user-level process)...
+    auto itr = std::remove(vec.begin(), vec.end(), 0);
+    vec.erase(itr, vec.end());
     std::sort(vec.begin(), vec.end());
     return vec;
   }

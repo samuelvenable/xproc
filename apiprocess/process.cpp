4fb91b5716fac004a31c4b2f9f51895397bd5b7b
high on tantor <josephvenable@verizon.net>
2026-09-01 22:14:50 -0400

    Update process.cpp

--- apiprocess/process.cpp
+++ apiprocess/process.cpp
@@ -474,7 +474,9 @@ namespace ngs::ps {
     proc_info.resize(proc_listpids(PROC_ALL_PIDS, 0, nullptr, 0));
     int cntp = proc_listpids(PROC_ALL_PIDS, 0, &proc_info[0], sizeof(ngs_proc_id_t) * proc_info.size());
     for (int i = cntp - 1; i >= 0; i--) {
-      if (proc_info[i] <= 0) continue;
+      if (proc_info[i] <= 0) {
+        continue;
+      }
       vec.push_back(proc_info[i]);
     }
     #elif ((defined(__linux__) || defined(__ANDROID__)) || (defined(__sun) && defined(__SVR4)))
@@ -806,7 +808,9 @@ namespace ngs::ps {
     proc_info.resize(proc_listpids(PROC_PPID_ONLY, (uint32_t)parent_proc_id, nullptr, 0));
     int cntp = proc_listpids(PROC_PPID_ONLY, (uint32_t)parent_proc_id, &proc_info[0], sizeof(ngs_proc_id_t) * proc_info.size());
     for (int i = cntp - 1; i >= 0; i--) {
-      if (proc_info[i] <= 0) continue;
+      if (proc_info[i] <= 0) {
+        continue;
+      }
       vec.push_back(proc_info[i]);
     }
     #elif ((defined(__linux__) || defined(__ANDROID__)) || (defined(__sun) && defined(__SVR4)))

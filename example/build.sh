fffd5d580dad43af1d08676effbec64ceaa4eb25
high on tantor <josephvenable@verizon.net>
2026-09-05 10:59:05 -0400

    Update and rename example.sh to build.sh

---
+++ example/build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+cd "${0%/*}";
+if [ "$OS" = "Windows_NT" ]; then
+  g++ main.cpp ../apiprocess/process.cpp -o xproc.exe -I. -std=c++17 -DNULLIFY_STDERR -static-libgcc -static-libstdc++ -static -lntdll -Wl,--subsystem,console; ./xproc.exe;
+elif [ `uname -s` = "Darwin" ]; then
+  clang++ main.cpp ../apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -mmacos-version-min=10.13 -arch arm64 -arch x86_64; ./xproc;
+elif [ `uname -s` = "Linux" ]; then
+  if [ -f "/bin/g++" ]; then
+    g++ main.cpp ../apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -static-libgcc -static-libstdc++ -static; ./xproc;
+  else
+    clang++ main.cpp ../apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR; ./xproc;
+  fi;
+elif [ `uname -s` = "FreeBSD" ]; then
+  clang++ main.cpp ../apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -lelf -lkvm -lpthread -static; ./xproc;
+elif [ `uname -s` = "DragonFly" ]; then
+  g++ main.cpp ../apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -static-libgcc -lkvm -lpthread -static; ./xproc;
+elif [ `uname -s` = "NetBSD" ]; then
+  g++ main.cpp ../apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -static-libgcc -lkvm -lpthread -static; ./xproc;
+elif [ `uname -s` = "OpenBSD" ]; then
+  clang++ main.cpp ../apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -lkvm -lpthread -static; ./xproc;
+elif [ `uname -s` = "SunOS" ]; then
+  if [ `uname -o` = "illumos" ]; then
+    g++ main.cpp ../apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -D__illumos__ -static-libgcc -lkvm -lproc; ./xproc;
+  else
+    g++ main.cpp ../apiprocess/process.cpp -o xproc -I. -std=c++17 -DNULLIFY_STDERR -static-libgcc -lkvm -lproc; ./xproc;
+  fi;
+fi;
